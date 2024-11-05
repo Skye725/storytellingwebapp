8cc9482c213a5a517e2694dc0a1347e8bccd3b96
Skye725 <101329664+Skye725@users.noreply.github.com>
2024-11-05 00:21:04 +0000

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,11 +4,11 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
+  title: 'Share your story',
+  description: 'Used to evaluate the quality of user-shared stories, assessing the strengths and weaknesses of the stories based on specific criteria and providing suggestions for revisions',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en-US',
+  default_language: 'zh-Hans',
 }
 
 export const API_PREFIX = '/api'
